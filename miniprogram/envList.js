779b80ca09ce9489d708b6ff6ba66af116952f4a
陈海俊 <353376352@qq.com>
2022-07-04 12:04:21 +0800

    initial

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-3gvcbogb97b3d216","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-3gvcbogb97b3d216 --n quickstartFunctions --r --project "E:\inkun" --report_first --report
